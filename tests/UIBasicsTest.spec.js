d8fed744e42988b8e58421d9fbe0134dca815398
Tanvir Hassan <101284808+RogerThatTan@users.noreply.github.com>
2025-07-23 12:28:31 +0600

    V4-Browser Context & Page

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -1,8 +1,16 @@
 const { test } = require('@playwright/test');
 //annonymus function doesnt need function name in modern JS
 
-test('First Playwright Test', async ({ browser }) => {
+test('Browser Context Playwright Test', async ({ browser }) => {
   //CHROME-plugins/cookies
 
-  const context = browser.newContext();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+});
+
+test('Page Playwright Test', async ({ page }) => {
+  await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+  //continue automation
 });
