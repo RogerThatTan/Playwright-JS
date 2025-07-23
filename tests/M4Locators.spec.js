21f2354e3535d08c3f16a5f262f03d9b5f956701
Tanvir Hassan <101284808+RogerThatTan@users.noreply.github.com>
2025-07-23 14:11:08 +0600

    V1-Locators & Type

---
+++ tests/M4Locators.spec.js
@@ -0,0 +1,25 @@
+const { test, expect } = require('@playwright/test');
+//annonymus function doesnt need function name in modern JS
+
+test.only('Browser Context Playwright Test', async ({ browser }) => {
+  //CHROME-plugins/cookies
+
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+  console.log(await page.title());
+
+  //css, Xpath
+
+  await page.locator('#username').fill('rahulshettyacadem');
+  await page.locator('[type=password]').fill('learning');
+  await page.locator('#signInBtn').click();
+});
+
+test('Page Playwright Test', async ({ page }) => {
+  await page.goto('https:google.com');
+  //get the title
+  console.log(await page.title());
+  await expect(page).toHaveTitle('Google');
+});
