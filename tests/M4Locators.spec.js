c95d07f860ede41eb1d96803f895a08166d8b5c7
Tanvir Hassan <101284808+RogerThatTan@users.noreply.github.com>
2025-07-23 17:23:30 +0600

    V3-Extract Text from Mutiple Locators

--- tests/M4Locators.spec.js
+++ tests/M4Locators.spec.js
@@ -1,4 +1,5 @@
 const { test, expect } = require('@playwright/test');
+const { single } = require('rxjs');
 //annonymus function doesnt need function name in modern JS
 
 test.only('Browser Context Playwright Test', async ({ browser }) => {
@@ -10,15 +11,23 @@ test.only('Browser Context Playwright Test', async ({ browser }) => {
   await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
   console.log(await page.title());
 
+  const userName = page.locator('#username');
+  const signIn = page.locator('#signInBtn');
+
   //css, Xpath
 
-  await page.locator('#username').fill('rahulshettyacadem');
+  await userName.fill('rahulshetty');
   await page.locator('[type=password]').fill('learning');
-  await page.locator('#signInBtn').click();
+  await signIn.click();
 
   //error is dynamic
   console.log(await page.locator("[style*='block']").textContent());
   await expect(page.locator("[style*='block']")).toContainText('Incorrect');
+
+  await userName.fill('');
+  await userName.fill('rahulshettyacademy');
+  await signIn.click();
+  console.log(await page.locator('.card-body a').nth(0).textContent());
 });
 
 test('Page Playwright Test', async ({ page }) => {
