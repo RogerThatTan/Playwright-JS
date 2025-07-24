691233966fc3abd56ef3286ede8fb54d145568a7
Tanvir Hassan <101284808+RogerThatTan@users.noreply.github.com>
2025-07-24 15:07:52 +0600

    V1-Dropdown & Radio

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -11,9 +11,20 @@ test('Browser Context Playwright Test', async ({ browser }) => {
   console.log(await page.title());
 });
 
-test.only('Page Playwright Test', async ({ page }) => {
+test('Page Playwright Test', async ({ page }) => {
   await page.goto('https:google.com');
   //get the title
   console.log(await page.title());
   await expect(page).toHaveTitle('Google');
+});
+
+test.only('UI Control Dropdown & Radio Test', async ({ page }) => {
+  await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+  const userName = page.locator('#username');
+  const signIn = page.locator('#signInBtn');
+  const dropDown = page.locator('select.form-control ');
+  await dropDown.selectOption('consult');
+  await page.locator('.radiotextsty').last().click();
+  await page.locator('#okayBtn').click();
+  await page.pause();
 });
