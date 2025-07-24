ee171c91d6915f1376b3576507f59f09d7933fa9
Tanvir Hassan <101284808+RogerThatTan@users.noreply.github.com>
2025-07-24 22:30:08 +0600

    M7V2-End-To-End

---
+++ tests/EndToEnd.spec.js
@@ -0,0 +1,33 @@
+const { test, expect } = require('@playwright/test');
+const { single } = require('rxjs');
+//annonymus function doesnt need function name in modern JS
+
+test('Browser Context Playwright Test', async ({ browser, page }) => {
+  await page.goto('https://rahulshettyacademy.com/client');
+  const productName = 'ZARA COAT 3';
+  const userName = page.locator('#userEmail');
+  const userPassword = page.locator('#userPassword');
+  const cardBody = await page.locator('.card-body b');
+  const products = page.locator('.card-body');
+
+  await userName.fill('anshika@gmail.com');
+  await userPassword.fill('Iamking@000');
+  await page.locator("[value='Login']").click();
+
+  //await page.waitForLoadState('networkidle'); //we are asking to wait till the network is idle so that we can get the AllTitle of the cardboard otherwise without this wait it will reutrn empoty array due to no proper loading
+  //alternate wait
+  await cardBody.first().waitFor();
+  const allTitles = await cardBody.allTextContents();
+  console.log(allTitles);
+
+  const count = await products.count();
+  for (let i = 0; i < count; i++) {
+    if ((await products.nth(i).locator('b').textContent()) === productName) {
+      //add to cart
+      await products.nth(i).locator('text= Add To Cart').click();
+      break;
+    }
+  }
+
+  //ZARA COAT 3
+});
