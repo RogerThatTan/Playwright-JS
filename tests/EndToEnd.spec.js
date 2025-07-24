387456d45f9a167e2fc16ed2c37b8c61914a0f53
Tanvir Hassan <101284808+RogerThatTan@users.noreply.github.com>
2025-07-25 01:40:55 +0600

    M7V6-E2E-Completed

--- tests/EndToEnd.spec.js
+++ tests/EndToEnd.spec.js
@@ -68,6 +68,17 @@ test('Browser Context Playwright Test', async ({ browser, page }) => {
   console.log(orderID);
 
   //now we will verify the order page that order is placed successfully
-
+  await page.locator("button[routerlink*='myorders']").click();
+  await page.locator('tbody').waitFor();
+  const rows = await page.locator('tbody tr');
+  for (let i = 0; i < (await rows.count()); i++) {
+    const rowOrderId = await rows.nth(i).locator('th').textContent();
+    if (orderID.includes(rowOrderId)) {
+      await rows.nth(i).locator('button').first().click();
+      break;
+    }
+  }
+  const orderIdDetails = await page.locator('.col-text').textContent();
+  expect(orderID.includes(orderIdDetails)).toBeTruthy();
   await page.pause();
 });
