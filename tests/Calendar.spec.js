ed6c466d5d25730aa23307a865004a493519732b
Tanvir Hassan <101284808+RogerThatTan@users.noreply.github.com>
2025-07-27 19:18:22 +0600

    M8V8- Calendar Assertions

--- tests/Calendar.spec.js
+++ tests/Calendar.spec.js
@@ -4,7 +4,7 @@ test('Calendar Validations', async ({ page }) => {
   const monthNumber = '6';
   const date = '15';
   const year = '2027';
-
+  const expectedList = [monthNumber, date, year];
   await page.goto('https://rahulshettyacademy.com/seleniumPractise/#/offers');
   await page.locator('.react-date-picker__wrapper').click();
   await page.locator('.react-calendar__navigation__label').click();
@@ -16,4 +16,12 @@ test('Calendar Validations', async ({ page }) => {
     .click();
 
   await page.locator("//abbr[text()='" + date + "']").click();
+
+  //assertions of calendar
+
+  const inputs = await page.locator('.react-date-picker__inputGroup input');
+  for (let i = 0; i < inputs.length; i++) {
+    const value = inputs[i].getAttribute('value');
+    expect(value).toEqual(expectedList[i]);
+  }
 });
