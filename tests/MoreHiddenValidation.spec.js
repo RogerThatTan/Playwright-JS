e98cac1317f4e2bd84ccd8c7aff0588cba5525e1
Tanvir Hassan <101284808+RogerThatTan@users.noreply.github.com>
2025-07-27 21:16:19 +0600

    M9V2- JS Pop Up & Mouse Hover

--- tests/MoreHiddenValidation.spec.js
+++ tests/MoreHiddenValidation.spec.js
@@ -6,7 +6,19 @@ test('Popup Validations', async ({ page }) => {
   //   await page.goBack(); //it will go back to rahul again from google
   //   await page.goForward(); //it will go to google from rahul
 
+  //this code block is for hide/show elements
   await expect(page.locator('#displayed-text')).toBeVisible();
   await page.locator('#hide-textbox').click();
   await expect(page.locator('#displayed-text')).toBeHidden();
+
+  //how to handle alert JS pop up which is know dialogue in PlayWright
+  await page.pause();
+
+  page.on('dialog', (dialog) => dialog.accept());
+  //   page.on('dialog', (dialog) => dialog.dismiss()); //to cancel/reject the pop up
+  await page.locator('#confirmbtn').click();
+
+  //Mouse Hover
+  await page.locator('#mousehover').hover();
+  await page.getByRole('link', { name: 'Top' }).click();
 });
