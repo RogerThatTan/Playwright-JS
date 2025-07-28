9452e8b22bb54996ae0e089532ffaeee6c8a195f
Tanvir Hassan <101284808+RogerThatTan@users.noreply.github.com>
2025-07-28 12:16:55 +0600

    M10V5- Create order validation

--- tests/WebAPI.spec.js
+++ tests/WebAPI.spec.js
@@ -1,11 +1,17 @@
 const { expect, test, request } = require('@playwright/test');
+const { application } = require('express');
+const { json } = require('stream/consumers');
 const loginPayLoad = {
   userEmail: 'anshika@gmail.com',
   userPassword: 'Iamking@000',
 };
-
+const orderPayload = {
+  orders: [{ country: 'Cuba', productOrderedId: '67a8dde5c0d3e6622a297cc8' }],
+};
 let token;
+let orderId;
 test.beforeAll(async () => {
+  //login API
   const apiContext = await request.newContext();
   const loginResponse = await apiContext.post(
     'https://rahulshettyacademy.com/api/ecom/auth/login',
@@ -18,73 +24,35 @@ test.beforeAll(async () => {
   // we need to parse it to extract the token
   token = loginResponseJson.token;
   console.log(token);
+
+  //
+  const orderResponse = await apiContext.post(
+    'https://rahulshettyacademy.com/api/ecom/order/create-order',
+    {
+      data: orderPayload,
+      headers: {
+        Authorization: token,
+        'Content-Type': 'application/json',
+      },
+    },
+  );
+  const orderResponseJson = await orderResponse.json();
+  console.log(orderResponse);
+  orderId = orderResponseJson.orders[0];
 });
 
 test.beforeEach(() => {});
 
+//Create order is successfull
 test('Browser Context Playwright Test', async ({ browser, page }) => {
   //adding token from the API into browser local storage.
   page.addInitScript((value) => {
     window.localStorage.setItem('token', value);
   }, token);
 
-  const productName = 'ZARA COAT 3';
-  const cardBody = await page.locator('.card-body b');
-  const products = page.locator('.card-body');
-  const email = 'anshika@gmail.com';
-
   //await page.waitForLoadState('networkidle'); //we are asking to wait till the network is idle so that we can get the AllTitle of the cardboard otherwise without this wait it will reutrn empoty array due to no proper loading
   //alternate wait
   await page.goto('https://rahulshettyacademy.com/client/#/auth/login');
-  await cardBody.first().waitFor();
-  const allTitles = await cardBody.allTextContents();
-  console.log(allTitles);
-
-  const count = await products.count();
-  // Adding ZARA COAT 3
-  for (let i = 0; i < count; i++) {
-    if ((await products.nth(i).locator('b').textContent()) === productName) {
-      //add to cart
-      await products.nth(i).locator('text= Add To Cart').click();
-      break;
-    }
-  }
-
-  //In the cart page now and checking the right product is added
-  await page.locator("[routerlink*='cart']").click();
-  await page.locator('div li').first().waitFor();
-  const bool = await page.locator("h3:has-text('ZARA COAT 3')").isVisible();
-  expect(bool).toBeTruthy();
-
-  //Now Lets checkout
-  await page.locator('text=Checkout').click();
-
-  await page.locator("[placeholder*='Country']").pressSequentially('ind');
-  const dropdown = page.locator('.ta-results');
-  await dropdown.waitFor();
-  const optionsCount = await dropdown.locator('button').count();
-  for (let i = 0; i < optionsCount; i++) {
-    const text = await dropdown.locator('button').nth(i).textContent();
-    if (text === ' India') {
-      await dropdown.locator('button').nth(i).click();
-      break;
-    }
-  }
-
-  //now lets verify the email address in the cart
-  expect(page.locator(".user__name [type='text']").first()).toHaveText(email);
-
-  await page.locator('.action__submit').click();
-
-  //Order complete verification
-  await expect(page.locator('.hero-primary')).toHaveText(
-    ' Thankyou for the order. ',
-  );
-
-  const orderID = await page
-    .locator('.em-spacer-1 .ng-star-inserted')
-    .textContent();
-  console.log(orderID);
 
   //now we will verify the order page that order is placed successfully
   await page.locator("button[routerlink*='myorders']").click();
@@ -92,12 +60,16 @@ test('Browser Context Playwright Test', async ({ browser, page }) => {
   const rows = await page.locator('tbody tr');
   for (let i = 0; i < (await rows.count()); i++) {
     const rowOrderId = await rows.nth(i).locator('th').textContent();
-    if (orderID.includes(rowOrderId)) {
+    if (orderId.includes(rowOrderId)) {
       await rows.nth(i).locator('button').first().click();
       break;
     }
   }
   const orderIdDetails = await page.locator('.col-text').textContent();
-  expect(orderID.includes(orderIdDetails)).toBeTruthy();
   await page.pause();
+
+  expect(orderId.includes(orderIdDetails)).toBeTruthy();
 });
+
+//Verify if order created is showing history page
+//Precondtion - create order - get order id
