5abd97762b9ad76283386a97edf68767acbeba55
Tanvir Hassan <101284808+RogerThatTan@users.noreply.github.com>
2025-08-02 21:33:36 +0600

    M13V4-ExcelJS-Write

--- ExcelJSUtil/excelDemo.js
+++ ExcelJSUtil/excelDemo.js
@@ -1,17 +1,26 @@
 const ExcelJs = require('exceljs');
 
 async function excelTest() {
+  let output = { row: -1, column: -1 };
   const workbook = new ExcelJs.Workbook();
   await workbook.xlsx.readFile('ExcelJSUtil/Data/excelDownloadTest.xlsx');
   const worksheet = workbook.getWorksheet('Sheet1');
 
   //print all the values in excel
 
-  worksheet.eachRow((row, rowNummber) => {
+  worksheet.eachRow((row, rowNumber) => {
     row.eachCell((cell, colNumber) => {
-      console.log(cell.value);
+      if (cell.value === 'Banana') {
+        output.row = rowNumber;
+        output.column = colNumber;
+      }
     });
   });
+
+  //write in the cell value
+  const cell = worksheet.getCell(output.row, output.column);
+  cell.value = 'Republic';
+  await workbook.xlsx.writeFile('ExcelJSUtil/Data/excelDownloadTest.xlsx');
 }
 
 excelTest();
