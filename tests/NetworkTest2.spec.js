49b06b05989c21f817ecd2eb60422d0bb1ad7bca
Tanvir Hassan <101284808+RogerThatTan@users.noreply.github.com>
2025-08-02 15:09:44 +0600

    M11V7-Access Unauthorize Order

---
+++ tests/NetworkTest2.spec.js
@@ -0,0 +1,29 @@
+const { test, expect } = require('@playwright/test');
+
+test('Security Test Request Intercept', async ({ page }) => {
+  //login
+  await page.goto('https://rahulshettyacademy.com/client');
+  const userName = page.locator('#userEmail');
+  const userPassword = page.locator('#userPassword');
+  const cardBody = await page.locator('.card-body b');
+  await userName.fill('anshika@gmail.com');
+  await userPassword.fill('Iamking@000');
+  await page.locator("[value='Login']").click();
+  await page.waitForLoadState('networkidle');
+  await cardBody.first().waitFor();
+
+  //login and reach orders page
+  await page.locator("button[routerlink*='myorders']").click();
+
+  await page.route(
+    'https://rahulshettyacademy.com/api/ecom/order/get-orders-details?id=*',
+    (route) =>
+      route.continue({
+        url: 'https://rahulshettyacademy.com/api/ecom/order/get-orders-details?id=621661f884b053f6765465b6',
+      }),
+  );
+  await page.locator("button:has-text('View')").first().click();
+  await expect(page.locator('p').last()).toHaveText(
+    'You are not authorize to view this order',
+  );
+});
