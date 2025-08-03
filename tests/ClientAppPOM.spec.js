4045f2d47745c43737bd235d31e9eb89e55d2f85
Tanvir Hassan <101284808+RogerThatTan@users.noreply.github.com>
2025-08-03 23:20:46 +0600

    M14V6-POM-Data Driven

--- tests/ClientAppPOM.spec.js
+++ tests/ClientAppPOM.spec.js
@@ -1,22 +1,23 @@
 const { test, expect } = require('@playwright/test');
 const { POManager } = require('../pageobjects/POManager.js');
+//Json->string->js object
+const dataSet = JSON.parse(
+  JSON.stringify(require('../utils/placeOrderTestData.json')),
+);
 
 //annonymus function doesnt need function name in modern JS
 test('Browser Context Playwright Test', async ({ page }) => {
   const poManager = new POManager(page);
   const loginPage = poManager.getLoginPage();
   const dashboardPage = poManager.getDashboardPage();
-  const email = 'universuswebtech@gmail.com';
-  const password = 'Iamking@000';
-  const productName = 'ZARA COAT 3';
   await loginPage.goTo();
-  await loginPage.validLogin(email, password);
-  await dashboardPage.searchProductAddCart(productName);
+  await loginPage.validLogin(dataSet.username, dataSet.password);
+  await dashboardPage.searchProductAddCart(dataSet.productName);
   await dashboardPage.navigateToCart();
 
   //In the cart page now and checking the right product is added
   const cartPage = poManager.getCartPage();
-  await cartPage.VerifyProductIsDisplayed(productName);
+  await cartPage.VerifyProductIsDisplayed(dataSet.productName);
   await cartPage.Checkout();
 
   const ordersReviewPage = poManager.getOrdersReviewPage();
