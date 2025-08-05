cb71a37fb6b8dee21399423eb2e61842fe25ad7f
Tanvir Hassan <101284808+RogerThatTan@users.noreply.github.com>
2025-08-05 13:46:41 +0600

    M19V5-Cucumber-World Constructor ->this.

--- features/step_defintions/steps.js
+++ features/step_defintions/steps.js
@@ -1,31 +1,50 @@
-const assert = require('assert');
 const { When, Then, Given } = require('@cucumber/cucumber');
-const { Greeter } = require('../../src');
-
+const { POManager } = require('../../pageobjects/POManager.js');
+const { expect } = require('@playwright/test');
+const playwright = require('@playwright/test');
 Given(
-  'a login to Ecommerece application with {username} and {password}',
-  function (username, password) {
+  'a login to Ecommerece application with {string} and {string}',
+  { timeout: 100 * 1000 },
+  async function (username, password) {
     // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    const browser = await playwright.chromium.launch({ headless: false });
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    this.poManager = new POManager(page);
+    const loginPage = this.poManager.getLoginPage();
+    await loginPage.goTo();
+    await loginPage.validLogin(username, password);
   },
 );
 
-When('Add {string} to Cart', function (string) {
+When('Add {string} to Cart', async function (productName) {
   // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+  this.dashboardPage = this.poManager.getDashboardPage();
+  await this.dashboardPage.searchProductAddCart(productName);
+  await this.dashboardPage.navigateToCart();
 });
 
-Then('Verify {string} is displayed in the Cart', function (string) {
+Then('Verify {string} is displayed in the Cart', async function (productName) {
   // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+  this.cartPage = this.poManager.getCartPage();
+  await this.cartPage.VerifyProductIsDisplayed(productName);
+  await this.cartPage.Checkout();
 });
 
-When('Enter valid details and Place the Order', function () {
+When('Enter valid details and Place the Order', async function () {
   // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+  this.ordersReviewPage = this.poManager.getOrdersReviewPage();
+  await this.ordersReviewPage.searchCountryAndSelect('ind', 'India');
+  this.orderId = await this.ordersReviewPage.SubmitAndGetOrderId();
+  console.log(this.orderId);
 });
 
-Then('Verify the order is present in the OrderHistory', function () {
+Then('Verify the order is present in the OrderHistory', async function () {
   // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+  await this.dashboardPage.navigateToOrders();
+  this.ordersHistoryPage = this.poManager.getOrdersHistoryPage();
+  await this.ordersHistoryPage.searchOrderAndSelect(this.orderId);
+  expect(
+    this.orderId.includes(await this.ordersHistoryPage.getOrderId()),
+  ).toBeTruthy();
 });
